f6eaf876b3669c71387586644e964b0cc8f72b5e
Amanda <adoyle@smu.edu>
2014-02-06 15:08:40 -0600

    Initial Commit

---
+++ github_test/A1AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  A1AppDelegate.h
+//  github_test
+//
+//  Created by Amanda D on 2/6/14.
+//  Copyright (c) 2014 Sharkbait. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface A1AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
