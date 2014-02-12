2d4005b838a9856f71bd8da9acbde79d10fe6687
Amanda <adoyle@smu.edu>
2014-02-12 16:26:50 -0600

    final assignment 1

---
+++ Assignment1/ButtonViewController.h
@@ -0,0 +1,13 @@
+//
+//  ButtonViewController.h
+//  Assignment1
+//
+//  Created by Kevin Donahoo on 2/6/14.
+//  Copyright (c) 2014 Sharkbait. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ButtonViewController : UIViewController
+
+@end
---
+++ Assignment1/ImageViewController.h
@@ -0,0 +1,15 @@
+//
+//  ImageViewController.h
+//  Assignment1
+//
+//  Created by Kevin Donahoo on 2/7/14.
+//  Copyright (c) 2014 Sharkbait. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewController : UIViewController
+@property (strong, nonatomic) NSString *imageName;
+@property (nonatomic) NSUInteger pageIndex;
+
+@end
