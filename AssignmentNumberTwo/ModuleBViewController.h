6819d8f0bcc1591b0ba4764b79b416430f41e78a
Amanda <adoyle@smu.edu>
2014-02-19 01:12:24 -0600

    assignment one show graphs from the microphone on module a and b has a machine link error

---
+++ AssignmentNumberTwo/ModuleBViewController.h
@@ -0,0 +1,14 @@
+//
+//  ModuleBViewController.h
+//  AssignmentNumberTwo
+//
+//  Created by Amanda D on 2/19/14.
+//  Copyright (c) 2014 Sharkbait. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ModuleBViewController : GLKViewController
+
+@end
